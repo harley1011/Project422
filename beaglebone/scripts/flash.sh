a7200fda0fd104e4177b93d660627592fed22519
harley1011 <harley.1011@gmail.com>
2016-11-23 15:25:21 -0500

    Adding beaglebone files

---
+++ beaglebone/scripts/flash.sh
@@ -0,0 +1,2 @@
+sudo diskutil unmount /dev/disk2s1 Volume sd on disk2s1 unmounted
+sudo dd bs=1m if=Downloads/bone-debian-8.6-lxqt-4gb-armhf-2016-11-06-4gb.img of=/dev/rdisk2
---
+++ beaglebone/scripts/internet_setup.sh
@@ -0,0 +1,2 @@
+sudo /sbin/route add default gateway 192.168.7.1
+sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf
