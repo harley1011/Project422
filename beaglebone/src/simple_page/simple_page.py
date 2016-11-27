8b31b080cc99c7deda2351483559a6b9e20c79a2
root <root@beaglebone.localdomain>
2016-11-27 00:16:22 +0000

    Adding template html files

--- beaglebone/src/simple_page/simple_page.py
+++ beaglebone/src/simple_page/simple_page.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, send_from_directory
 from jinja2 import TemplateNotFound
 
 simple_page = Blueprint('simple_page', __name__,
@@ -10,9 +10,20 @@ def show_index(page):
         return render_template('pages/%s.html' % page, name='Harley')
     except TemplateNotFound:
         abort(404)
+
+@simple_page.route('/assets/css/<path:path>')
+def serve_css(path):
+    print path
+    try:
+        return send_from_directory('assets/css', path)
+    except:
+        abort(404)
+
 @simple_page.route('/<page>')
 def show(page):
     try:
         return render_template('pages/%s.html' % page)
     except TemplateNotFound:
         abort(404)
+
+
