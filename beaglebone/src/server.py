c01d57a48206ba30823e8445fc016d9a9c6cd9c3
harley1011 <harley.1011@gmail.com>
2016-12-03 11:37:55 -0500

    Making template dynamic

--- beaglebone/src/server.py
+++ beaglebone/src/server.py
@@ -1,42 +1,57 @@
-from flask import Flask
-from simple_page.simple_page import simple_page
+from flask import Flask, Blueprint, render_template, abort, send_from_directory
+from jinja2 import TemplateNotFound
 
 # import socketio
 # import eventlet
 # import eventlet.wsgi
-from comms import serial_reader
+#from comms import serial_reader
 import thread
 
 #sio = socketio.Server()
 
+#comms = serial_reader.SerialReader()
+#thread.start_new_thread(comms.start, ())
+
 app = Flask(__name__)
+
+simple_page = Blueprint('simple_page', __name__,
+                        template_folder='templates')
+
+
+@simple_page.route('/', defaults={'page': 'index'})
+def show_index(page):
+    try:
+        return render_template('pages/%s.html' % page, name='Harley')
+    except TemplateNotFound:
+        abort(404)
+
+
+@simple_page.route('/assets/<path:path>')
+def serve_css(path):
+    print(path)
+    try:
+        return send_from_directory('templates\\pages\\assets\\', path)
+    except:
+        abort(404)
+
+
+@simple_page.route('/<page>')
+def show(page):
+    try:
+        return render_template('pages/%s' % page, nodes=['1', '2', '3', '4', '5'])
+    except TemplateNotFound:
+        abort(404)
+
 app.register_blueprint(simple_page)
 # Blueprint can be registered many times
 app.register_blueprint(simple_page, url_prefix='/pages')
 
-comms = serial_reader.SerialReader()
-thread.start_new_thread(comms.start, ())
-
-
-# @sio.on('connect', namespace='/chat')
-# def connect(sid, environ):
-#     print("connect ", sid)
-#
-#
-# @sio.on('chat message', namespace='/chat')
-# def message(sid, data):
-#     print("message ", data)
-#     sio.emit('reply', room=sid)
-#
-#
-# @sio.on('disconnect', namespace='/chat')
-# def disconnect(sid):
-#     print('disconnect ', sid)
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
   # app = socketio.Middleware(sio, app)
   #
   # eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8000)), app)
+
 
 
