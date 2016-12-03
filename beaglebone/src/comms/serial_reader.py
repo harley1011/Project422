76fe3e25642890c9aa329117b07a4c322cfcd83f
harley1011 <harley.1011@gmail.com>
2016-12-03 11:22:09 -0500

    Moving files around and worker on serial_reader

--- beaglebone/src/comms/serial_reader.py
+++ beaglebone/src/comms/serial_reader.py
@@ -1,52 +1,63 @@
 # Serial2 TX = pin 21 on P9 header
 # Serial2 RX = pin 22 on P9 header
-
-import SocketServer
-import sys
-
+import time
 from bbio import *
 
-online_node_ids = []
-data_buffer = ''
 
-def setup():
-    print "Setting up serial communications..."
-    Serial2.begin(9600)
+class SerialReader:
 
-def loop():
-    if (Serial2.available()):
-        # There's incoming data
+    def __init__(self):
+        self.nodes = []
+        self.data_buffer = ''
+        self.status = 'Offline'
 
-        while(Serial2.available()):
-            data += Serial2.read()
-            delay(5)
 
-        # Print what was sent:
-        print "Data received:\n  '%s'" % data
-        data_buffer += data
-        parse_message()
+    def setup(self):
+        print "Setting up serial communications..."
+        Serial2.begin(9600)
 
-    delay(200)
+    def loop(self):
+        data = ''
+        if (Serial2.available()):
+            # There's incoming data
 
-def parse_message():
-    # Handle new node register
-    if message[0] == 0:
-    # Message is expected to be 5 bytes
-        handle_new_node(data_buffer[:5])
-        data_buffer = data_buffer[5:]
-    else:
-        handle_node_message(data_buffer[:12])
-        data_buffer = data_buffer[12:]
+            while(Serial2.available()):
+                data += Serial2.read()
+                delay(5)
 
-def handle_new_node(message):
-    current_id = online_node_ids[len(online_node_ids) - 1]
-    message[0] = chr(current_id + 1)
-    Serial2.write(message)
+            # Print what was sent:
+            print "Data received:\n  '%s'" % data
+            self.data_buffer += data
+            self.parse_message()
+        delay(200)
 
-def handle_node_message(message):
-    id = message[0]
-    info = message[1]
+    def parse_message(self):
+        # Handle new node register
+        if message[0] == 0 and len(self.data_buffer) >= 5:
+            handle_new_node(self.data_buffer[:5])
+            self.data_buffer = data_buffer[5:]
+        elif len(self.data_buffer) >= 12:
+            handle_node_message(self.data_buffer[:12])
+            self.data_buffer = self.data_buffer[12:]
 
-def start():
-    run(setup, loop)
+    def handle_new_node(self, message):
+        current_id = self.nodes[len(online_node_ids) - 1].id
+        message[0] = chr(current_id + 1)
+        self.nodes.append(Node(current_id + 1))
+        Serial2.write(message)
+
+    def handle_node_message(self, message):
+        id = message[0]
+        info = message[1]
+
+    def start(self):
+        run(setup, loop)
+
+
+class Node:
+    def __init__(self, id, name, status):
+        self.id = id
+        self.name = name
+        self.status = status
+        self.last_receive = time.time()
 
--- beaglebone/src/server.py
+++ beaglebone/src/server.py
@@ -1,40 +1,42 @@
 from flask import Flask
 from simple_page.simple_page import simple_page
 
-import socketio
-import eventlet
-import eventlet.wsgi
-#from comms import serial_reader
+# import socketio
+# import eventlet
+# import eventlet.wsgi
+from comms import serial_reader
 import thread
 
-sio = socketio.Server()
+#sio = socketio.Server()
 
 app = Flask(__name__)
 app.register_blueprint(simple_page)
 # Blueprint can be registered many times
 app.register_blueprint(simple_page, url_prefix='/pages')
 
-#thread.start_new_thread(serial_reader.start, ())
+comms = serial_reader.SerialReader()
+thread.start_new_thread(comms.start, ())
 
 
-@sio.on('connect', namespace='/chat')
-def connect(sid, environ):
-    print("connect ", sid)
-
-
-@sio.on('chat message', namespace='/chat')
-def message(sid, data):
-    print("message ", data)
-    sio.emit('reply', room=sid)
-
-
-@sio.on('disconnect', namespace='/chat')
-def disconnect(sid):
-    print('disconnect ', sid)
+# @sio.on('connect', namespace='/chat')
+# def connect(sid, environ):
+#     print("connect ", sid)
+#
+#
+# @sio.on('chat message', namespace='/chat')
+# def message(sid, data):
+#     print("message ", data)
+#     sio.emit('reply', room=sid)
+#
+#
+# @sio.on('disconnect', namespace='/chat')
+# def disconnect(sid):
+#     print('disconnect ', sid)
 
 if __name__=='__main__':
-  app = socketio.Middleware(sio, app)
-
-  eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8000)), app)
+    app.run(host='0.0.0.0')
+  # app = socketio.Middleware(sio, app)
+  #
+  # eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8000)), app)
 
 
