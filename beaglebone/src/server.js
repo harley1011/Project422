30a0525dfb4d4431e4c98dc2d3194310051b144e
harley1011 <harley.1011@gmail.com>
2016-12-02 19:44:58 -0500

    Moving files around

--- beaglebone/src/server.js
+++ beaglebone/src/server.js
@@ -1,10 +1,29 @@
 var express = require('express')
 var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        console.log(msg);
+        io.emit('chat message', msg);
+    });
+});
+app.use(express.static(__dirname + '/templates'));
+
+app.use(express.static(__dirname + '/templates/assets'));
+
+http.listen(5000, function () {
+  console.log('Example app listening on port 5000!')
 })
+
+
