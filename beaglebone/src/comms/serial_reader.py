1c9917db2b767c198f62d346b674ecd2cb51a8de
root <root@beaglebone.localdomain>
2016-12-21 06:20:42 +0000

    Working on ligt sensor and timeout

--- beaglebone/src/comms/serial_reader.py
+++ beaglebone/src/comms/serial_reader.py
@@ -32,8 +32,9 @@ class SerialReader:
             print "Data received:\n  '%s'" % ":".join("{:02x}".format(ord(c)) for c in data)
             self.data_buffer += data
             self.parse_message()
+        self.check_timeouts()
         delay(200)
-
+        
     def parse_message(self):
         # Handle new node register
         if self.data_buffer[0] == '0' and len(self.data_buffer) >= 7:
@@ -60,33 +61,42 @@ class SerialReader:
         new_message[0] = chr(current_id + 49) 
         self.nodes.append(Node(current_id + 1, 'Node ' + new_message[0], 'Registering'))
         return_message = ''.join(new_message)
-        print "Returning " + ":".join("{:02x}".format(ord(c)) for c in return_message)
+        print "Adding new node and returning " + ":".join("{:02x}".format(ord(c)) for c in return_message)
         Serial2.write(return_message)
 
     def handle_node_message(self, message):
-        print "Handling Node Message"
         id = int(message[0])
         node = self.nodes[id - 1]
-        self.status = "Online"
+        node.status = "Online"
+        node.timeout_timer = time.time()
         info = message[1]
-        if info == '0': # Handle door
-            self.handle_door(self, node, message[2:])
-        elif info == '1': # Handle light
-            self.handle_light(self, node, message[2:])
-        elif info == '2': # Handle temp
-            self.handle_temp(self, node, message[2:])
+        if info == '1': # Handle door
+            self.handle_door(node, message[2:])
+        elif info == '2': # Handle light
+            self.handle_light(node, message[2:])
+        elif info == '3': # Handle temp
+            self.handle_temp(node, message[2:])
+
+
+    def check_timeouts(self):
+        for node in self.nodes:
+            if time.time() - node.timeout_timer > 20:
+                node.status = "Offline"
 
     def handle_door(self, node, data):
-        print "Handle Motion" 
-        if data[0] == 1:
+        print "Handle Motion"
+        d = hex(ord(data[0]))
+        if d != '0x1':
             node.data = "Motion Detected"
         else:
             node.data = "No Motion"
-        node.status = "Online"
         node.type = "Motion"
     
     def handle_light(self, node, data):
-        node.data = data
+        print "Handling light"
+        d = int(ord(data[3]) << 24 | ord(data[2]) << 16 | ord(data[1]) << 8 | ord(data[0]))
+        node.type = "Light"
+        node.data = d
 
     def handle_temp(self, node, data):
         node.data = data
@@ -103,7 +113,6 @@ class Node:
         self.name = name
         self.status = status
         self.type = 'NA'
-        self.last_receive = time.time()
         self.data = 0
-        self.timer = None
+        self.timeout_timer = None
 
